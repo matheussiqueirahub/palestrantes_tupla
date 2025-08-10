fe9eeb739590d151b17a4d168cc1d372ff6190fc
Matheus Siqueira <matheushstor@gmail.com>
2025-08-10 12:41:41 -0300

    feat: atividade prática 4 - tupla de palestrantes

---
+++ palestrantes.py
@@ -0,0 +1,12 @@
+# Tupla com informações dos palestrantes
+palestrantes = (
+    ('Ana Souza', 'Inteligência Artificial', 'Universidade Federal de Pernambuco'),
+    ('Carlos Lima', 'Cibersegurança', 'Instituto de Tecnologia de São Paulo'),
+    ('Mariana Oliveira', 'Desenvolvimento Web', 'Universidade de Brasília')
+)
+
+# Exibindo informações do terceiro palestrante
+terceiro = palestrantes[2]
+print('Nome:', terceiro[0])
+print('Tema:', terceiro[1])
+print('Instituição:', terceiro[2])
